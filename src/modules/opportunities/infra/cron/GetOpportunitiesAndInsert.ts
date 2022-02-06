855efabb2e5398d53ac9b19121a626147a597e3a
Nypeeh <lammerlammer@outlook.com>
2022-02-06 19:25:41 -0300

    feat: Add function to queue job

--- src/modules/opportunities/infra/cron/GetOpportunitiesAndInsert.ts
+++ src/modules/opportunities/infra/cron/GetOpportunitiesAndInsert.ts
@@ -1,9 +1,7 @@
 import cron from 'node-cron'
 
+import Queues from '@shared/infra/bull/Queues'
 import { PipedriveProvider } from '@modules/opportunities/providers/OpportunityProvider/implementations/PipedriveProvider'
-import { BlingProvider } from '@modules/opportunities/providers/BlingProvider/implementations/BlingProvider'
-import { SaveOpportunityService } from '@modules/opportunities/services/SaveOpportunityService'
-import { container } from 'tsyringe'
 
 async function GetOpportunitiesAndInsert() {
   console.log(' == CRON STARTED == GetOpportunitiesAndInsert')
@@ -15,31 +13,15 @@ async function GetOpportunitiesAndInsert() {
   if (!wonOpportunities[0]) {
     return
   }
-  const blingProvider = new BlingProvider()
+
+  console.log({ wonOpportunities })
 
   // Register opportunities on Bling
-  for (const opportunity of wonOpportunities) {
-    await blingProvider.createOrder({
-      code: Math.random(),
-      clientName: opportunity.person_name,
-      value: opportunity.value,
-      description: opportunity.title,
-    })
-
-    // Save in DATABASE
-    const saveOpportunity = container.resolve(SaveOpportunityService)
-
-    await saveOpportunity.execute({
-      value: opportunity.value,
-      currency: opportunity.currency,
-      person_name: opportunity.person_name,
-      title: opportunity.title,
-    })
-  }
+  await Queues.add('InsertOpportunitiesJob', wonOpportunities)
 
   console.log(' == CRON ENDED == GetOpportunitiesAndInsert')
 }
 
-export default cron.schedule('0 */24 * * *', GetOpportunitiesAndInsert, {
+export default cron.schedule('10 * * * *', GetOpportunitiesAndInsert, {
   scheduled: false,
 })
